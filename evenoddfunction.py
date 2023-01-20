1c2d469b2cc9fd77050e323ad6ee2fb756f40b01
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-01-20 12:09:48 +0530

    Add files via upload

---
+++ evenoddfunction.py
@@ -0,0 +1,4 @@
+lst=[1,2,3,4,5,6,7,8,9,10]
+
+evennumbers =list(filter(lambda x : (x%2==0),lst))
+print(evennumbers)
---
+++ functioncalling.py
@@ -0,0 +1,13 @@
+def abc(a,b):
+      c=a+b
+      #print(a)
+      #print(b)
+      return c
+      
+a= int(input("ENTER THE VALUE OF A:"))  
+b= int(input("ENTER THE VALUE OF B:"))      
+
+print("SUM IS ",abc(a,b))
+    
+
+      
