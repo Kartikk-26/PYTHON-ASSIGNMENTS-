2c1be1b0265e0cbba57daaff3cf175ac78885145
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-01-20 10:58:56 +0530

    Add files via upload

---
+++ smallgreater.py
@@ -0,0 +1,11 @@
+a = int(input("ENTER THE NUMBER\n"))
+b = int(input("ENTER THE NUMBER\n"))
+if(a<b):
+      print("B IS GREATER")
+      print(b)
+elif a==b:
+      print("A IS EQUAL TO B")
+else:
+    print("A IS GREATER")
+    print(a)
+
